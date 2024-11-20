799f2c1b71deb95f4b6776cdc465fb6cd0341fde
Mikolaj Pujanek <m.p.pujanek@student.tue.nl>
2024-11-20 14:18:18 +0100

    added Driver class, pending updates with visibility class

---
+++ Driver.py
@@ -0,0 +1,28 @@
+import GraphGen
+import networkx as nx
+import ParkingSpotState
+
+class Driver:
+
+    def __init__(self, G: GraphGen, position):
+        self.G = G
+        self.position = position
+        self.kb = {node : ParkingSpotState.UNKNOWN for node in self.G.nodes}
+
+    def move(self, new_position):
+        # move to new position if there exists an edge to it, and if the node is not occupied by a car 
+        if self.G.has_edge(self.position, new_position) and not self.G.nodes[new_position].get('occupied'):
+            self.position = new_position
+            return True
+        else: 
+            return False
+        # the returns are for feedback to the strategy (error handling)
+        
+    def updateInformation(self):
+    # for each currently visible node
+        # if node state is unknown in kb
+            # self.kb[node] = self.G.nodes[node].get('state')
+        pass
+
+    def getInformation(self):
+        return self.kb
