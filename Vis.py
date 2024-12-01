4a43c95e246864663b5c3a903094968e9f619247
N-Kinderdijk <114729829+N-Kinderdijk@users.noreply.github.com>
2024-12-02 00:46:46 +0100

    Added vis stuff. Getting neighbours doesn't seem to work.

---
+++ Vis.py
@@ -0,0 +1,33 @@
+import ParkingGraph
+import networkx as nx
+from ParkingSpotState import ParkingSpotState
+
+class Vis:
+
+    def __init__(self, G: ParkingGraph):
+        self.G = G.getGraph()
+        self.kb = {node : ParkingSpotState.UNKNOWN for node in self.G.nodes}
+
+    
+    def getNeighbors(self, position):
+        neighbors = [node for node in self.G.nodes if self.G.has_edge(position, node)]
+        return neighbors
+
+    # Will run a BFS to find the first visible cars.
+    def getNewCars(self, position):
+        queue = [position]
+        while len(queue) > 0:
+            newCars = []
+
+            v = queue.pop
+            #Getting neighbours seems to be an issue.
+            for neighbour in self.getNeighbors(v):
+                #Parking spot not yet explored
+                print(neighbour)
+                if self.kb[neighbour] == ParkingSpotState.UNKNOWN or self.kb[neighbour] == ParkingSpotState.EMPTY:
+                    queue.append(neighbour)
+                else:
+                    #nx.get_node_attributes(self.G, "")
+                    self.G.Graph.nodes[neighbour].get("occupied")
+                    newCars.append(neighbour)
+        return newCars
